da54f3e4b8422d499dffd007247eae00af4778da
jamilmahmudlu <jamilmahmudlu@gmail.com>
2023-08-04 20:04:13 +0400

    python sorting done

---
+++ main.py
@@ -0,0 +1,45 @@
+"""
+Bubble sort:
+23 42 16 81 59 70
+
+Level 1
+step 1 -> '23' '42' 16 81 59 70
+step 2 -> 23 '16' '42' 81 59 70 
+step 3-> 23 16 '42' '81' 59 70
+step 4 -> 23 16 42 '59' '81' 70
+step 5 -> 23 16 42 59 '70' '81'
+
+23 16 42 59 70 81
+_
+Level 2
+step 1 -> '16' '23' 42 59 70 81
+___
+"""
+
+"""
+Selection sort
+
+18 72 9 23 16 54 46 99 87
+
+step 1 -> 9 72 18 23 16 54 46 99 87
+step 2 -> 9 16 18 23 72 54 46 99 87
+step 3 -> deyishmir
+step 4 -> deyishmir
+step 5 -> 9 16 18 23 46 54 72 99 87
+step 6 -> deyishmir
+step 7 -> deyishmir
+step 8 -> 9 16 18 23 46 54 72 87 99
+"""
+
+'''
+Insertion sort
+
+29 32 11 56 45 78 68 99 81
+step 1 -> deyishmir
+step 2 -> 11 29 32 56 45 78 68 99 81
+step 3 -> deyishmir
+step 4 -> 11 29 32 45 56 78 68 99 81
+step 5 -> deyishmir
+step 6 -> 11 29 32 45 56 68 78 99 81
+step 7 -> deyishmir
+'''
